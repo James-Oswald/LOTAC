c18030ff38090130756979ac545c97d7a12a262b
James Oswald <jamesoswald111@gmail.com>
2026-08-12 04:27:19 +0000

    Small rewrite

--- LOTAC2/Models.lean
+++ LOTAC2/Models.lean
@@ -10,6 +10,22 @@ open Textbook
 
 #doc (Manual) "Frames and Models" =>
 
+:::details "Notation"
+To handle the notation for the various uses of the turnstile symbol, we define a typeclass `Models` that represents the satisfaction relation between two types, along with its negation. This allows us to use the turnstile symbol `⊨` to denote satisfaction and `⊭` to denote non-satisfaction in a consistent manner across different contexts.
+
+```lean
+class Models (α : Type u) (β : Type v) where
+  models : α → β → Prop
+infixl:51 (priority := high) " ⊨ " => Models.models
+
+@[simp]
+def Models.not_models {α : Type u} {β : Type v}
+[Models α β] (a : α) (b : β) : Prop :=
+  ¬(a ⊨ b)
+infixl:51 (priority := high) " ⊭ " => Models.not_models
+```
+:::
+
 :::definition "Frames and Models"
 
 A frame is a pair $`(S, R)` where `S` is a non-empty set of worlds and $R$ is
@@ -24,6 +40,7 @@ structure Frame where
 
 A Φ-model is a pair $`(F, V)` where `F` is a frame and `V` is a valuation
 function that assigns to each propositional variable a set of worlds in `F`.
+We represent models as an extension of frames with a valuation function.
 
 ```lean
 variable {Φ : Type}
@@ -33,6 +50,18 @@ structure Model extends Frame where
 ```
 :::
 
+Another useful notion is that of a pointed model, which is a model together
+with a distinguished world in the model. We will use pointed models to define
+the satisfaction relation for formulas in a model. Formally, we define pointed
+models as a dependent pair of a model and a world in that model.
+
+```lean
+structure PointedModel {Φ : Type} extends @Model Φ where
+  w : S
+notation "⟨" M ", " w "⟩ₘ" => PointedModel.mk M w
+
+```
+
 :::definition "Satisfaction in a Model"
 Let $`M = (F, V)` be a Φ-model and let $`w` be a world in `F`.
 We define the satisfaction relation $`M, w \vDash φ` for a formula `φ`
@@ -40,18 +69,21 @@ inductively as follows.
 
 ```lean
 @[simp]
-def Model.satisfies (M : @Model Φ) (w : M.S) : L Φ → Prop
-| .atom p => M.V p w
+def PointedModel.satisfies (M : @PointedModel Φ) : L Φ → Prop
+| .atom p => M.V p M.w
 | ⊥ₜ       => False
-| φ →ₜ ψ   => M.satisfies w φ → M.satisfies w ψ
-| □ₜφ     => ∀ v : M.S, (M.R w v) → (M.satisfies v φ)
+| φ →ₜ ψ   => M.satisfies φ → M.satisfies ψ
+| □ₜφ      => ∀ w' : M.S, M.R M.w w' →
+    (⟨M.toModel, w'⟩ₘ.satisfies φ)
 
-notation  M "⊨[" w "]" φ => Model.satisfies M w φ
+instance : Models (@PointedModel Φ) (L Φ) where
+  models := PointedModel.satisfies
 
-def Model.not_satisfies (M : @Model Φ) (w : M.S) (φ : L Φ) : Prop :=
-  ¬Model.satisfies M w φ
-
-notation  M " ⊭[" w "] " φ => Model.not_satisfies M w φ
+-- This helper allows us to use the notation freely in simplification.
+@[simp]
+theorem PointedModel.satisfies_def (M : @Model Φ) (w : M.S)  (φ : L Φ) :
+(⟨M, w⟩ₘ ⊨ φ) ↔ (PointedModel.mk M w).satisfies φ := by
+  rfl
 ```
 
 The derived connectives are defined as follows.
@@ -59,31 +91,40 @@ The derived connectives are defined as follows.
 ```lean
 @[simp]
 theorem Model.satisfies_neg (M : @Model Φ) (w : M.S) (φ : L Φ) :
-(M ⊭[w] φ) ↔ (M ⊨[w] ¬ₜ φ) := by
+(⟨M, w⟩ₘ ⊭ φ) ↔ (⟨M, w⟩ₘ ⊨ ¬ₜφ) := by
   rfl
 
 @[simp]
 theorem Model.satisfies_and (M : @Model Φ) (w : M.S) (φ ψ : L Φ) :
-(M ⊨[w] φ ∧ₜ ψ) ↔ (M ⊨[w] φ) ∧ (M ⊨[w] ψ) := by
-  simp only [L.and, L.not, satisfies, imp_false, Classical.not_imp, not_not]
+(⟨M, w⟩ₘ ⊨ (φ ∧ₜ ψ)) ↔ (⟨M, w⟩ₘ ⊨ φ) ∧ (⟨M, w⟩ₘ ⊨ ψ) := by
+  simp only [L.and, L.not, PointedModel.satisfies_def, PointedModel.satisfies,
+    imp_false, Classical.not_imp, not_not]
 
 @[simp]
 theorem Model.satisfies_or (M : @Model Φ) (w : M.S) (φ ψ : L Φ) :
-(M ⊨[w] φ ∨ₜ ψ) ↔ (M ⊨[w] φ) ∨ (M ⊨[w] ψ) := by
-  simp only [L.or, L.not, L.and, satisfies]
-  grind only [#8eb5]
+(⟨M, w⟩ₘ ⊨ (φ ∨ₜ ψ)) ↔ (⟨M, w⟩ₘ ⊨ φ) ∨ (⟨M, w⟩ₘ ⊨ ψ) := by
+  simp; grind only [#aec4]
 
 @[simp]
 theorem Model.satisfies_iff (M : @Model Φ) (w : M.S) (φ ψ : L Φ) :
-(M ⊨[w] φ ↔ₜ ψ) ↔ ((M ⊨[w] φ) ↔ (M ⊨[w] ψ)) := by
-  simp only [L.iff, L.and, L.not, satisfies]
-  grind only [#5ede, #004e]
+(⟨M, w⟩ₘ ⊨ (φ ↔ₜ ψ)) ↔ ((⟨M, w⟩ₘ ⊨ φ) ↔ (⟨M, w⟩ₘ ⊨ ψ)) := by
+  simp; grind only [#14f9, #7704, #4195]
 
 @[simp]
 theorem Model.satisfies_dia (M : @Model Φ) (w : M.S) (φ : L Φ) :
-(M ⊨[w] ◇ₜ φ) ↔ ∃ v : M.S, (M.R w v) ∧ (M ⊨[v] φ) := by
-  simp only [L.dia, L.not, satisfies]
-  grind only [#3e50, #0e31]
+(⟨M, w⟩ₘ ⊨ (◇ₜ φ)) ↔ ∃ v : M.S, (M.R w v) ∧ (⟨M, v⟩ₘ ⊨ φ) := by
+  classical
+  simp only [Models.models, L.dia, L.not, PointedModel.satisfies]
+  apply Iff.intro
+  · intro h
+    by_contra hnone
+    apply h
+    intro v hv hφ
+    apply hnone
+    exact ⟨v, hv, hφ⟩
+  · intro h hbox
+    rcases h with ⟨v, hv, hφ⟩
+    exact hbox v hv hφ
 ```
 :::
 
@@ -255,8 +296,11 @@ $$`M ⊨ φ ↔ ∀ w ∈ M.S, M, w ⊨ φ`
 ```lean
 @[simp]
 def L.true_in_model (φ : L Φ) (M : @Model Φ) : Prop :=
-  ∀ w : M.S, M ⊨[w] φ
-notation  M " ⊨ " φ => L.true_in_model φ M
+  ∀ w : M.S, ⟨M, w⟩ₘ ⊨ φ
+
+@[simp]
+instance : Models (@Model Φ) (L Φ) where
+  models M φ := φ.true_in_model M
 ```
 
 A formula φ is valid in a frame F if it is true in every model based on F.
@@ -265,8 +309,10 @@ $$`F ⊨ φ ↔ ∀ M, M.toFrame = F → M ⊨ φ`
 @[simp]
 def L.valid_in_frame (φ : L Φ) (F : Frame) : Prop :=
   ∀ M : @Model Φ, M.toFrame = F → M ⊨ φ
-notation  F " ⊨ " φ => L.valid_in_frame φ F
 
+@[simp]
+instance : Models Frame (L Φ) where
+  models F φ := φ.valid_in_frame F
 ```
 
 A formula φ is valid in a class of frames C if it is valid in every frame in C.
@@ -276,71 +322,58 @@ In lean we will reprsent a class as a set of frames.
 @[simp]
 def L.valid_in_class (φ : L Φ) (C : Set Frame) : Prop :=
   ∀ F : Frame, F ∈ C → F ⊨ φ
-notation  C " ⊨ " φ => L.valid_in_class φ C
+
+@[simp]
+instance : Models (Set Frame) (L Φ) where
+  models C φ := φ.valid_in_class C
 ```
 
-We say a formula is valid if it is true in all models,
+We say a formula *is valid* if it is true in all models,
 or equivalently, valid in the class of all frames.
 ```lean
 @[simp]
 def L.valid (φ : L Φ) : Prop :=
   ∀ M : @Model Φ, M ⊨ φ
-prefix:max "⊨" => L.valid
+prefix:max "⊨ " => L.valid
 
 theorem L.valid_iff_valid_in_class (φ : L Φ) :
-(⊨ φ) ↔ Set.univ ⊨ φ := by
-  simp only [L.valid, L.valid_in_class, Set.mem_univ]
-
+(⊨ φ) ↔ (Set.univ : Set Frame) ⊨ φ := by
+  simp only [valid, Models.models, true_in_model, valid_in_class,
+    Set.mem_univ, valid_in_frame, forall_const]
+  apply Iff.intro
+  · intro a F M a_1 w
+    subst a_1
+    simp_all only
+  · intro a M w
+    simp_all only
 ```
 
 
-We have analgous definitions for truth and validity over schema.
-IE a schema is true in a model if every instance of the schema is
+We have analogous definitions for truth and validity over schema.
+A schema is true in a model if every instance of the schema is
 true in the model.
 
 ```lean
 @[simp]
-def Schema.true (Γ : Set (L Φ)) (M : @Model Φ) : Prop :=
+def Schema.true_in_model (Γ : Set (L Φ)) (M : @Model Φ) : Prop :=
   ∀ φ ∈ Γ, M ⊨ φ
-notation  M " ⊨ " Γ => Schema.true Γ M
-
-@[simp]
-def Schema.false (Γ : Set (L Φ)) (M : @Model Φ) : Prop :=
-  ¬(M ⊨ Γ)
-notation  M " ⊭ " Γ => Schema.false Γ M
+instance : Models (@Model Φ) (Set (L Φ)) where
+  models M Γ := Schema.true_in_model Γ M
 
 @[simp]
 def Schema.valid (Γ : Set (L Φ)) (F : Frame) : Prop :=
   ∀ M : @Model Φ, M.toFrame = F → M ⊨ Γ
-notation  F " ⊨ " Γ => Schema.valid Γ F
+instance : Models Frame (Set (L Φ)) where
+  models F Γ := Schema.valid Γ F
 
 @[simp]
 def Schema.validInClass (Γ : Set (L Φ)) (C : Set Frame) : Prop :=
   ∀ F : Frame, F ∈ C → F ⊨ Γ
-notation  C " ⊨ " Γ => Schema.validInClass Γ C
+instance : Models (Set Frame) (Set (L Φ)) where
+  models C Γ := Schema.validInClass Γ C
 ```
 :::
 
-It is useful to also define the negation of truth and validity using the
-$`⊭` notation.
-:::details "Negations"
-```
-@[simp]
-def L.false_in_model (φ : L Φ) (M : @Model Φ) : Prop :=
-  ¬(M ⊨ φ)
-notation  M " ⊭ " φ => L.false_in_model φ M
-
-@[simp]
-def L.not_valid_in_frame (φ : L Φ) (F : Frame) : Prop :=
-  ¬(F ⊨ φ)
-notation  F " ⊭ " φ => L.not_valid_in_frame φ F
-
-@[simp]
-def L.not_validInClass (φ : L Φ) (C : Set Frame) : Prop :=
-  ¬(C ⊨ φ)
-notation  C " ⊭ " φ => L.not_validInClass φ C
-```
-:::
 
 
 *Exercises*
@@ -356,49 +389,72 @@ notation  C " ⊭ " φ => L.not_validInClass φ C
 
 :::details "Solutions"
 ```lean
-example (M : @Model Φ) : M ⊨ □ₜ⊤ₜ := by
-  simp_all only [L.true, Model.satisfies, L.top, L.not, implies_true]
+example (M : @Model Φ) : M ⊨ (□ₜ(⊤ₜ : L Φ)) := by
+  change ∀ w : M.S, ⟨M, w⟩ₘ ⊨ (□ₜ(⊤ₜ : L Φ))
+  intro w v hv hbot
+  exact hbot
 
-example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (□ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ □ₜψ)) := by
-  simp_all only [L.true, Model.satisfies, implies_true]
+example (M : @Model Φ) (φ ψ : L Φ) :
+M ⊨ (□ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ □ₜψ)) := by
+  change ∀ w : M.S,
+    ⟨M, w⟩ₘ ⊨ (□ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ □ₜψ))
+  intro w hbox hφ v hv
+  exact hbox v hv (hφ v hv)
 
-example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (◇ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ ◇ₜψ)) := by
-  simp_all only [L.true, Model.satisfies, L.dia, L.not,
-    imp_false, Classical.not_imp, not_forall, not_and,
-    not_not, forall_exists_index, and_imp]
-  intro w x a a_1 a_2
-  simp_all only [forall_const]
-  apply Exists.intro
-  · apply And.intro
-    on_goal 2 => { exact a_1 }
-    · simp_all only
+example (M : @Model Φ) (φ ψ : L Φ) :
+M ⊨ (◇ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ ◇ₜψ)) := by
+  change ∀ w : M.S,
+    ⟨M, w⟩ₘ ⊨ (◇ₜ(φ →ₜ ψ) →ₜ (□ₜφ →ₜ ◇ₜψ))
+  intro w hdia hbox
+  rcases (M.satisfies_dia w (φ →ₜ ψ)).mp hdia with ⟨v, hv, himp⟩
+  exact (M.satisfies_dia w ψ).mpr ⟨v, hv, himp (hbox v hv)⟩
 
-example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (□ₜ(φ →ₜ ψ) →ₜ (◇ₜφ →ₜ ◇ₜψ)) := by
-  simp_all
-  intro w a x a_1 a_2
-  apply Exists.intro
-  · apply And.intro
-    · exact a_1
-    · simp_all only
+example (M : @Model Φ) (φ ψ : L Φ) :
+M ⊨ (□ₜ(φ →ₜ ψ) →ₜ (◇ₜφ →ₜ ◇ₜψ)) := by
+  change ∀ w : M.S,
+    ⟨M, w⟩ₘ ⊨ (□ₜ(φ →ₜ ψ) →ₜ (◇ₜφ →ₜ ◇ₜψ))
+  intro w hbox hdia
+  rcases (M.satisfies_dia w φ).mp hdia with ⟨v, hv, hφ⟩
+  exact (M.satisfies_dia w ψ).mpr ⟨v, hv, hbox v hv hφ⟩
 
-example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (□ₜ(φ ∧ₜ ψ) ↔ₜ (□ₜφ ∧ₜ □ₜψ)) := by
-  simp_all only [L.true, L.iff, L.and, L.not, Model.satisfies,
-    imp_false, Classical.not_imp, not_not, implies_true,
-    not_true_eq_false, not_forall, not_exists, not_and]
-
-example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (◇ₜ(φ ∨ₜ ψ) ↔ₜ (◇ₜφ ∨ₜ ◇ₜψ)) := by
-  simp only [L.true, Model.satisfies_iff]
+example (M : @Model Φ) (φ ψ : L Φ) :
+M ⊨ (□ₜ(φ ∧ₜ ψ) ↔ₜ (□ₜφ ∧ₜ □ₜψ)) := by
+  change ∀ w : M.S, ⟨M, w⟩ₘ ⊨ (□ₜ(φ ∧ₜ ψ) ↔ₜ (□ₜφ ∧ₜ □ₜψ))
   intro w
+  rw [Model.satisfies_iff, Model.satisfies_and]
+  constructor
+  · intro h
+    constructor
+    · intro v hv
+      exact (M.satisfies_and v φ ψ).mp (h v hv) |>.left
+    · intro v hv
+      exact (M.satisfies_and v φ ψ).mp (h v hv) |>.right
+  · intro h v hv
+    exact (M.satisfies_and v φ ψ).mpr ⟨h.left v hv, h.right v hv⟩
+
+example (M : @Model Φ) (φ ψ : L Φ) :
+M ⊨ (◇ₜ(φ ∨ₜ ψ) ↔ₜ (◇ₜφ ∨ₜ ◇ₜψ)) := by
+  change ∀ w : M.S, ⟨M, w⟩ₘ ⊨ (◇ₜ(φ ∨ₜ ψ) ↔ₜ (◇ₜφ ∨ₜ ◇ₜψ))
+  intro w
+  rw [Model.satisfies_iff, Model.satisfies_or]
   apply Iff.intro
   · case mp =>
-    intro a
-    have ⟨w', h1, h2⟩ := (M.satisfies_dia w (φ ∨ₜ ψ)).mp a
-    simp_all only [Model.satisfies_or]
-    aesop
+    intro h
+    rcases (M.satisfies_dia w (φ ∨ₜ ψ)).mp h with ⟨v, hv, hφψ⟩
+    cases (M.satisfies_or v φ ψ).mp hφψ with
+    | inl hφ => exact Or.inl ((M.satisfies_dia w φ).mpr ⟨v, hv, hφ⟩)
+    | inr hψ => exact Or.inr ((M.satisfies_dia w ψ).mpr ⟨v, hv, hψ⟩)
   · case mpr =>
-    intro a
-    simp_all only [Model.satisfies_or]
-    aesop
+    intro h
+    cases h with
+    | inl hφ =>
+      rcases (M.satisfies_dia w φ).mp hφ with ⟨v, hv, hφv⟩
+      exact (M.satisfies_dia w (φ ∨ₜ ψ)).mpr
+        ⟨v, hv, (M.satisfies_or v φ ψ).mpr (Or.inl hφv)⟩
+    | inr hψ =>
+      rcases (M.satisfies_dia w ψ).mp hψ with ⟨v, hv, hψv⟩
+      exact (M.satisfies_dia w (φ ∨ₜ ψ)).mpr
+        ⟨v, hv, (M.satisfies_or v φ ψ).mpr (Or.inr hψv)⟩
 ```
 :::
 
@@ -414,6 +470,6 @@ example (M : @Model Φ) (φ ψ : L Φ) : M ⊨ (◇ₜ(φ ∨ₜ ψ) ↔ₜ (◇
 :::details "Solutions"
 ```lean
 example (M : @Model Φ) (φ : L Φ) : M ⊭ (□ₜφ →ₜ φ) := by
-
+  sorry
 ```
 :::
