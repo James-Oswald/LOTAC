eb77fa3094ed35f50f70aa906c2fd6a3232b5c40
James Oswald <jamesoswald111@gmail.com>
2026-08-18 15:50:53 -0400

    landing page

--- MDML.lean
+++ MDML.lean
@@ -20,6 +20,25 @@ textbook "Many Dimensional Modal Logics : Theory and Applications"
 {cite Gabbay2003b}[]. The book also pulls heavily from Goldblatt's
 "Logics of Time and Computation"{cite Goldblatt1992}[].
 
+*Philosophy*
+
+MDML takes a _Deep Embedding_ approach to formalizing many-dimensional modal
+logics in Lean, meaning that we explicitly represent the syntax and semantics of
+the logic within the proof assistant, rather than relying on Lean's built-in
+logical framework.
+
+*Citing*
+
+If you use MDML in your research or teaching, please cite it as follows:
+
+```
+@book{Oswald2024,
+  author    = {James Oswald},
+  title     = {Many Dimensional Modal Logics In Lean},
+  year      = {2026},
+}
+```
+*Contents*
 
 {include 1 MDML.PML}
 :::::::
